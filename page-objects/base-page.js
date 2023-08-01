a1d31b4a07fb177be7b717a1199a02d67a7b5cb6
iurii <iurii@IURII-PC>
2023-08-01 06:24:33 -0500

    introduced base page

---
+++ page-objects/base-page.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const test = require('@playwright/test');
+
+exports.BasePage =class BasePage {
+  constructor(page) {
+    this.page = page;
+  }
+  async navigate() {
+    await this.page.goto(process.env.BASE_URL);
+  }
+}
